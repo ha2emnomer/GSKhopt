d3e695c47f94d54252f810710b5fc599370db0b6
Hazem Nomer <impulselabs2012@gmail.com>
2021-12-08 13:55:59 +0200

    gskhopt0.4

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GSKhopt",
+    version="0.0.4",
+    author="Hazem Nomer",
+    author_email="h.nomer@nu.edu.eg",
+    description="GSK algorithm for efficient hyperparameter optimization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ha2emnomer/GSKHpot/",
+    project_urls={
+        "Bug Tracker": "https://github.com/ha2emnomer/GSKHpot/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires=">=3.6",
+)
